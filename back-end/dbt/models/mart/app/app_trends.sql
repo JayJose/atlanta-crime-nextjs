1e4481d532ab7bbeed7725200fafd45af82dc4e0
John Murray <johnmurray@fastmail.com>
2022-10-16 11:25:38 -0400

    adds dense trend data

--- back-end/dbt/models/mart/app/app_trends.sql
+++ back-end/dbt/models/mart/app/app_trends.sql
@@ -1,24 +1,41 @@
+--- for each neighborhood, each week_of_year, each offense...
+
 with cutoff as (
     select day_of_year
     from {{ ref('dim_dates') }}
-    where date = '2022-08-15'
+    where date = '2022-09-30'
+),
+neighborhoods as (
+    select distinct neighborhood from {{ ref('dim_neighborhoods') }}
+),
+offenses as (
+    select distinct offense from {{ ref('dim_offenses') }}
+),
+periods as (
+    select d.year,
+        d.week_of_year,
+        d.id as date
+    from {{ ref('dim_dates') }} as d
+        inner join cutoff as c on d.day_of_year <= c.day_of_year
+    where d.year in (2021, 2022)
+),
+dense as (
+    select *
+    from neighborhoods
+    cross join offenses
+    cross join periods
 )
-select n.neighborhood,
-    o.offense,
-    o.offense_category,
+select d.neighborhood,
+    d.offense,
     d.year,
-    d.month,
-    d.month_name_abbreviated,
-    count(f.crime_tk)
-from {{ ref('dim_dates') }} as d
-    inner join {{ ref('fct_crimes') }} as f on f.date_id = d.id
-    inner join cutoff as c on d.day_of_year <= c.day_of_year
-    inner join {{ ref('dim_offenses') }} as o on o.id = f.offense_id
-    inner join {{ ref('dim_neighborhoods') }} as n on n.id = f.neighborhood_id
-where d.year in (2021, 2022)
-group by n.neighborhood,
-    o.offense,
-    o.offense_category,
+    d.week_of_year,
+    count(distinct f.crime_tk) as value
+from dense as d
+left join {{ ref('fct_crimes') }} as f
+    on f.neighborhood = d.neighborhood
+    and f.offense = d.offense
+    and f.date_id = d.date
+group by d.neighborhood,
+    d.offense,
     d.year,
-    d.month,
-    d.month_name_abbreviated
+    d.week_of_year
