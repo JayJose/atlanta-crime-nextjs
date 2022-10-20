d9684b26cc48e3ec3cf9ea69af5e09c57fd208d3
John Murray <johnmurray@fastmail.com>
2022-10-20 11:16:42 -0400

    audUbon -> audObon

--- back-end/dbt/models/staging/stg_crimes.sql
+++ back-end/dbt/models/staging/stg_crimes.sql
@@ -19,8 +19,6 @@ select md5(offense_id || lower(ibr_code)) as crime_tk,
     lower(uc2_literal) as uc2_literal,
     case
         when lower(neighborhood) = 'westminster/milmar' then 'westminster/milar'
-        when lower(neighborhood) = 'audobon forest' then 'audubon forest'
-        when lower(neighborhood) = 'audobon forest west' then 'audubon forest west'
         when lower(neighborhood) = 'pomona park' then 'the villages at east lake'
         else lower(coalesce(neighborhood, 'none'))
     end as neighborhood,
