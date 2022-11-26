464e6fa696f7332f3db0fea7f67d09f8af039793
John Murray <johnmurray@fastmail.com>
2022-11-26 10:17:50 -0500

    adds jest setup

---
+++ front-end/test/stack.test.js
@@ -0,0 +1,33 @@
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+
+  get peek() {
+    return this.items[this.top];
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+}
+
+describe('My Stack', () => {
+  let stack;
+  beforeEach(() => (stack = new Stack()));
+
+  it('is created empty', () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it('it can push to the top', () => {
+    stack.push('XXX');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('XXX');
+  });
+
+  it.todo('is can pop off');
+});
